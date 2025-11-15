a69865ba17c9f840fdba542634369fe1b159eb6b
Antonia <go35huq@matlab.rbg.tum.de>
2025-11-15 13:17:49 +0000

    ex 1 and 2 working: mention check 2 for compelx values for QAM and PSK if case

--- code_coded_modulation/basic-lab-code/+modulation/map_to_constellation.m
+++ code_coded_modulation/basic-lab-code/+modulation/map_to_constellation.m
@@ -1,35 +1,15 @@
-unction [seq_out] = map_to_constellation(seq_in,X,label)
-%MAP_TO_CONSTELLATION This function maps a binary input sequence to a
-%sequence of symbols from a given constellation using a specified label.
-%
-% Inputs: 
-%   seq_in: binary input sequence of length n given as a 1xn vector of type
-%           uint8.
-%   X:      vector of size 1xM containing the M-symbols of the given
-%           constellation.
-%   label:  matrix of size Mxm containing the binary labels of the M
-%           symbols. Note that the input length n is always defined as a
-%           multiple of m
-%
-% Outputs:
-%   seq_out: vector of size 1x(n/m) containing the output sequence
+function [seq_out] = map_to_constellation(seq_in,X,label)
 
-
-number_of_symbols = length(seq_in)/size(label,2); % lenght of seq_in / number of bits per symbol
-seq_out = zeros(1,number_of_symbols);  
-
-for k = 1:number_of_symbols
-    
-    %Extract m bits for from seq_in for the current symbol
-    bits = seq_in((k-1)*m+1 : k*m);
-
-    % Find the index of the  matching row in the label coloumn vector
-    idx = find (all(label = bits),2); % all(A,2) tests the rows of A, find() gives the index
-
-    % Get the corresponding symbol from the row vector X and write it into seq_out in the correponding index
-    seq_out(k) = X(idx);
-end       
-
-
+m = size(label,2);
+no_symb = length(seq_in)/m;
+bits = seq_in(1:1+m-1); 
+for i = 1:size(label,1)
+    idx = find(ismember(label, bits,'rows'));
 end
-
+seq_out(1) = X(idx); 
+for i = 1+m:m:length(seq_in) 
+    bits = seq_in(i:i+m-1); 
+    idx = find(ismember(label, bits,'rows'));
+    seq_out(i-(m-1)) = X(idx);    
+end      
+end
