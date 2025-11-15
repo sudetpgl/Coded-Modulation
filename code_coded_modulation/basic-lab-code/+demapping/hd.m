a6c1f7f33864b4eebb33ec4c6be17fbf9c5c5a2e
Antonia <go35huq@matlab.rbg.tum.de>
2025-11-15 14:36:01 +0000

    ex 3 with a problem on bits 7 8 of output

--- code_coded_modulation/basic-lab-code/+demapping/hd.m
+++ code_coded_modulation/basic-lab-code/+demapping/hd.m
@@ -3,6 +3,15 @@ function [seq_out] = hd(seq_in,X,label)
 %   Detailed explanation goes here
 
 seq_out = zeros(1,length(seq_in)*size(label,2));
+m = size(label,2);
+mapped = [];
+for i = 1:length(X)
+    for j = 1:length(seq_in)
+        diff(j) = abs(X(i)-seq_in(j));
+    end
+    [~,min_pos] = min(diff);
+   seq_out( (min_pos*m-1) : (min_pos*m) ) = label(i,:);
+end
 
 end
 
