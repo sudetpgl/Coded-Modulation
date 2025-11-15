8d3591698b3ebea634f7fee0ff61b6a60b0d2c60
Antonia <go35huq@matlab.rbg.tum.de>
2025-11-15 13:36:48 +0000

    same coments as last commit, now working

--- code_coded_modulation/basic-lab-code/+modulation/map_to_constellation.m
+++ code_coded_modulation/basic-lab-code/+modulation/map_to_constellation.m
@@ -1,5 +1,20 @@
 function [seq_out] = map_to_constellation(seq_in,X,label)
+%MAP_TO_CONSTELLATION This function maps a binary input sequence to a
+%sequence of symbols from a given constellation using a specified label.
+%
+% Inputs: 
+%   seq_in: binary input sequence of length n given as a 1xn vector of type
+%           uint8.
+%   X:      vector of size 1xM containing the M-symbols of the given
+%           constellation.
+%   label:  matrix of size Mxm containing the binary labels of the M
+%           symbols. Note that the input length n is always defined as a
+%           multiple of m
+%
+% Outputs:
+%   seq_out: vector of size 1x(n/m) containing the output sequence
 
+seq_out = zeros(1,length(seq_in)/size(label,2));
 m = size(label,2);
 no_symb = length(seq_in)/m;
 bits = seq_in(1:1+m-1); 
@@ -7,9 +22,11 @@ for i = 1:size(label,1)
     idx = find(ismember(label, bits,'rows'));
 end
 seq_out(1) = X(idx); 
+count = 1;
 for i = 1+m:m:length(seq_in) 
     bits = seq_in(i:i+m-1); 
     idx = find(ismember(label, bits,'rows'));
-    seq_out(i-(m-1)) = X(idx);    
+    seq_out(i-count) = X(idx);   
+    count = count + 1;
 end      
 end
